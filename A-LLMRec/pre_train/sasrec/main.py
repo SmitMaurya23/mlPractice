abbe6b91f04158636938dc16eaf589a762ce1665
SmitMaurya23 <131233128+SmitMaurya23@users.noreply.github.com>
2024-10-20 15:28:31 +0530

    Update main.py

--- A-LLMRec/pre_train/sasrec/main.py
+++ A-LLMRec/pre_train/sasrec/main.py
@@ -38,6 +38,7 @@ if __name__ == '__main__':
     cc = 0.0
     for u in user_train:
         cc += len(user_train[u])
+    print('average sequence length: %.2f' % (cc / len(user_train)))
     
     
     # dataloader
